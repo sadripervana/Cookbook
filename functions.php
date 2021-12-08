4e128958f965370db43b1653dede2875b997a77e
sadripervana <sadripervana@gmail.com>
2021-12-08 16:28:35 +0100

    new

---
+++ functions.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Dates & Times</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1 class="title"><a href="index.php"><</a> Dates & Times </h1>
+	<a id="top" href="#bottom" ><h1>Go bottom</h1></a>
+
+  <br>
+  <a id="topp" href="#bottom" ><h1>Go bottom</h1></a>
+  <a id="bottomm" href="#top" ><h1>Go up</h1></a>
+  <a id="bottom" href="#top"><h1>Go Up</h1></a>
+</body>
+</html>
